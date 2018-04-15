5c3655a42c5cb959083f47a18ce5b85b06ba5d1e
emirhanaydin <emirhanaydin@users.noreply.github.com>
2018-04-15 17:58:14 +0300

    Project initialization

---
+++ Client/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            Console.Write("IP Address: ");
+            var ipaddrInput = Console.ReadLine();
+
+            if (!IPAddress.TryParse(ipaddrInput ?? throw new InvalidOperationException(), out var ipaddr))
+                if (ipaddrInput.Equals("localhost", StringComparison.OrdinalIgnoreCase))
+                    ipaddr = IPAddress.Parse("127.0.0.1");
+                else
+                    throw new InvalidOperationException("IP Address is not valid.");
+
+            Console.Write("Port number: ");
+            var portInput = Console.ReadLine();
+
+            if (!ushort.TryParse(portInput ?? throw new InvalidOperationException(), out var port))
+                throw new InvalidOperationException("Port number is not valid.");
+
+            client.Connect(ipaddr, port);
+
+            Console.WriteLine("Connected to the server. Write \"exit\" to close.");
+
+            while (true)
+            {
+                var inputCommand = Console.ReadLine();
+
+                if (string.Equals(inputCommand, "exit", StringComparison.OrdinalIgnoreCase)) break;
+
+                client.Send(Encoding.ASCII.GetBytes(inputCommand ?? throw new InvalidOperationException()));
+
+                var buffReceived = new byte[128];
+                var numReceived = client.Receive(buffReceived);
+
+                Console.WriteLine("Received: " + Encoding.ASCII.GetString(buffReceived, 0, numReceived));
+            }
+
+            client.Shutdown(SocketShutdown.Both);
+            client.Close();
+            client.Dispose();
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            var ipaddr = IPAddress.Any;
+            var ipep = new IPEndPoint(ipaddr, 23000);
+
+            listenerSocket.Bind(ipep);
+            listenerSocket.Listen(5);
+            Console.WriteLine("Waiting client...");
+            var client = listenerSocket.Accept();
+            Console.WriteLine("Client has connected: {0} (IP End Point: {1})", client, client.RemoteEndPoint);
+
+            var buff = new byte[128];
+            while (true)
+            {
+                int numBytes;
+                try
+                {
+                    numBytes = client.Receive(buff);
+                }
+                catch (SocketException)
+                {
+                    break;
+                }
+
+                Console.WriteLine("Number of received bytes: " + numBytes);
+
+                var receivedText = Encoding.ASCII.GetString(buff, 0, numBytes);
+                if (receivedText.Equals("exit", StringComparison.OrdinalIgnoreCase)) break;
+
+                Console.WriteLine("Received data: " + receivedText);
+
+                client.Send(buff);
+
+                Array.Clear(buff, 0, buff.Length);
+            }
+
+            client.Close();
+            client.Dispose();
+        }
+    }
+}
